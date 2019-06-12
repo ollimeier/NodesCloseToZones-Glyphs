062ad572e6c915373d839fd2aa8f342f711beb13
Olli Meier <hallo@olli-meier.de>
2019-06-12 11:31:08 +0200

    Bugfix
    Upload bugfix. Nodes were not displayed if negative zone.

--- NodesCloseToZones.glyphsReporter/Contents/Resources/plugin.py
+++ NodesCloseToZones.glyphsReporter/Contents/Resources/plugin.py
@@ -19,20 +19,29 @@ from math import atan2, cos, pi, sin, degrees
 tolerance = 4
 
 def closeToArea(tol, pos, size, y):
-	bottom = pos
-	top = pos + size
-	if size < 0:
-		bottom = pos - size
-		top = pos
-	else:
-		bottom = pos
-		top = pos + size
+
+	if size >= 0:
+		pos1 = pos - tol
+		pos2 = pos
+
+		size1 = pos + size
+		size2 = size1 + tol
 	
-	if y < bottom and y + tol > bottom:
-		return True
-	if y > top and y - tol < top:
-		return True
-	return False
+		if y >= pos1 and y < pos2 or y > size1 and y <= size2:
+			return True
+		else:
+			return False
+	else:
+		pos1 = pos
+		pos2 = pos + tol	
+
+		size1 = pos + size - tol
+		size2 = pos + size 
+	
+		if (y > pos1 and y <= pos2) or (y >= size1 and y < size2):
+			return True
+		else:
+			return False
 
 def closeEnough(value1, value2, tolerance):
 	diff = value1 - value2
